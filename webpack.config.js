5c71a40100f0f69c4fb7a79475fa233ffc8190dc
JoelMuaka <joel.muakansilulu.etu@univ-lille.fr>
2022-12-18 23:56:47 +0100

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const PRODUCTION = false;
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/scripts/main.js"),
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "scripts/bundle.js",
+  },
+
+  mode: PRODUCTION ? "production" : "development",
+  devtool: PRODUCTION ? undefined : "eval-source-map",
+
+  devServer: {
+    static: {
+      publicPath: path.resolve(__dirname, "dist"),
+      watch: true,
+    },
+    host: "localhost",
+    port: 8888,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.(png|jpg|gif)/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        /*{
+          from: './src/html/*.html',
+          to:  'html/[name].html'
+        },
+  */
+        {
+          from: "./src/images/*",
+          to: "images/[name].[ext]",
+        },
+        {
+          from: "./src/style/*",
+          to: "style/[name].[ext]",
+        },
+      ],
+    }),
+  ],
+};
